505abcfd9025d4b906a451ff89e7b236d418d1de
MihailPetkov96 <mpetkov228@gmail.com>
2023-09-13 00:23:15 +0300

    Add todo.js

---
+++ src/todo.js
@@ -0,0 +1,21 @@
+const todos = [
+    {
+        title: 'Wash clothes',
+        description: 'Do the laundry for the whole household before noon.',
+        dueDate: '22/09/2023',
+        priority: 'medium',
+    },
+    {
+        title: 'Go grocery shopping',
+        description: 'Buy milk, bread, water, eggs and vegetables.',
+        dueDate: '24/09/2023',
+        priority: 'high',
+    },
+    {
+        title: 'Sweep porch',
+        description: 'Sweep front porch to make house look better.',
+        dueDate: '25/09/2023',
+        priority: 'low',
+    },
+];
+
